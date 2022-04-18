f13b5132de648fcd4bc093713bf523e39600f943
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-04-18 19:04:27 -0300

    adicionando router product

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
 
 const ProductsController = require('../controllers/products')
+const ClientsController = require('../controllers/clients')
 
 router.get('/client/:id?', ProductsController.get)
 router.post('/client', ProductsController.post)
 router.put('/client/:id', ProductsController.put)
 router.delete('/client/:id', ProductsController.remove)
+
+router.get('/product/:id?', ClientsController.get)
+router.post('/product', ClientsController.post)
+router.put('/product/:id', ClientsController.put)
+router.delete('/product/:id', ClientsController.remove)
+
 
 module.exports = router
